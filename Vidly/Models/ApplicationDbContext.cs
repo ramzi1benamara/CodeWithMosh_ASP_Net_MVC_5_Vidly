f0d2f3d12be0837cacbca0efe4eedf17373fbf94
Sophart <phunsophart@gmail.com>
2020-09-19 16:15:01 +0700

    Add-Migration InitalModel

--- Vidly/Models/ApplicationDbContext.cs
+++ Vidly/Models/ApplicationDbContext.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace Vidly.Models
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Customer> Customers { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
