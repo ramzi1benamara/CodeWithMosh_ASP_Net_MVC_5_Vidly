9927386ef03eaf075576ce8ee9de7481c8162555
Sophart <phunsophart@gmail.com>
2020-09-19 12:54:40 +0700

    Try Action Results

--- Vidly/Controllers/MovieController.cs
+++ Vidly/Controllers/MovieController.cs
@@ -12,9 +12,13 @@ namespace Vidly.Controllers
         // GET: Movie/Random
         public ActionResult Random()
         {
-            var movie = new Movie(){Name = "Shrek!"};
+            var movie = new Movie() { Name = "Shrek!" };
 
-            return View(movie);
+            //            return View(movie);
+            //            return Content("Hello, Sophart");
+            //            return HttpNotFound();
+            //            return new EmptyResult();
+            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
         }
     }
 }
