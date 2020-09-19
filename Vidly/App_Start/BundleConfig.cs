7121a18c09cf224e8285d89d3fd3aa32b89f8cd1
Sophart <phunsophart@gmail.com>
2020-09-19 12:47:03 +0700

    Add Lumen Bootstrap Theme

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace Vidly
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-lumen.min.css",
                       "~/Content/site.css"));
         }
     }
