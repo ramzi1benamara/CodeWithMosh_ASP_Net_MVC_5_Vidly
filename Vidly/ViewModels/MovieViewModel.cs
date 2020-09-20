ba047ad09bcea5c6f77e49f2a90b21436a6cee1b
Sophart <phunsophart@gmail.com>
2020-09-20 21:35:03 +0700

    Add MovieViewModel

---
+++ Vidly/ViewModels/MovieViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.ViewModels
+{
+    public class MovieViewModel
+    {
+        public Movie Movie { get; set; }
+
+        public IEnumerable<Genre> Genres { get; set; }
+
+        public string Title => Movie.Id == 0 ? "New" : "Edit";
+    }
+}
