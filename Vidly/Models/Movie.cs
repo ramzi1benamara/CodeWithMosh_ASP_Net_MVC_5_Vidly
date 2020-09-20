48f5ba5d18a38231cd63d6be528444a7847d7664
Sophart <phunsophart@gmail.com>
2020-09-20 21:48:06 +0700

    Apply Attributes to Movie Model

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,13 +10,17 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
 
         public byte NumberInStock { get; set; }
 
-        public DateTime DateAdded { get; set; }
+        [Required]
+        public DateTime? DateAdded { get; set; }
 
-        public DateTime ReleaseDate { get; set; }
+        [Required]
+        public DateTime? ReleaseDate { get; set; }
 
         public Genre Genre { get; set; }
 
