cb2d63d01873625312a6ae321361561fc445462b
Sophart <phunsophart@gmail.com>
2020-09-20 12:03:28 +0700

    Initial Movie Model to Db

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -8,6 +8,17 @@ namespace Vidly.Models
     public class Movie
     {
         public int Id { get; set; }
+
         public string Name { get; set; }
+
+        public byte NumberInStock { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public Genre Genre { get; set; }
+
+        public byte GenreId { get; set; }
     }
 }
