e73081ef42d36ed4214155044f7031d260c453e1
Sophart <phunsophart@gmail.com>
2020-09-20 14:06:47 +0700

    Add CustomerViewModel

---
+++ Vidly/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.ViewModels
+{
+    public class CustomerViewModel
+    {
+        public Customer Customer { get; set; }
+
+        public IEnumerable<MembershipType> MembershipTypes { get; set; }
+
+        public string Title => Customer.Id == 0 ? "New" : "Edit";
+    }
+}
