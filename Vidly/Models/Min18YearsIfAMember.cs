8cb8d4e06d05261fcd140fef9dbef37867eb09f0
Sophart <phunsophart@gmail.com>
2020-09-21 10:25:28 +0700

    Refactor Magic Number in Min18YearsIfAMember

--- Vidly/Models/Min18YearsIfAMember.cs
+++ Vidly/Models/Min18YearsIfAMember.cs
@@ -12,7 +12,8 @@ namespace Vidly.Models
         {
             var customer = (Customer)validationContext.ObjectInstance;
 
-            if (customer.MembershipTypeId == 1 || customer.MembershipTypeId == 0)
+            if (customer.MembershipTypeId == MembershipType.PayAsYouGo
+                || customer.MembershipTypeId == MembershipType.Unknown)
                 return ValidationResult.Success;
 
             if (customer.BirthDate == null)
