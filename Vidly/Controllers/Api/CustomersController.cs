04fcef8d1cc28eeed1391a34b69fcdc66d12c05a
Sophart <phunsophart@gmail.com>
2020-10-02 22:25:18 +0700

    Add Customer Api Controller

---
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+        private ApplicationDbContext _dbContext;
+
+        public CustomersController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _dbContext.Dispose();
+        }
+
+        [HttpGet]
+        public IList<Customer> GetCustomers()
+        {
+            return _dbContext.Customers.ToList();
+        }
+    }
+}
