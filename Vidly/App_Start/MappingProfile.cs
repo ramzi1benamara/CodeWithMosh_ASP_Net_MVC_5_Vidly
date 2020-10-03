04f3474ba4f4d5d623a394d568b1cbe1ebfa7f3d
Sophart <phunsophart@gmail.com>
2020-10-03 15:35:32 +0700

    Config MappingProfile to Let AutoMapper Ignore Ids

--- Vidly/App_Start/MappingProfile.cs
+++ Vidly/App_Start/MappingProfile.cs
@@ -12,10 +12,12 @@ namespace Vidly.App_Start
     {
         public MappingProfile()
         {
-            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<Customer, CustomerDto>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
             Mapper.CreateMap<CustomerDto, Customer>();
 
-            Mapper.CreateMap<Movie, MovieDto>();
+            Mapper.CreateMap<Movie, MovieDto>()
+                .ForMember(m=>m.Id, opt=>opt.Ignore());
             Mapper.CreateMap<MovieDto, Movie>();
         }
     }
