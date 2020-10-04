0c9cc02d34330439f8fa378f3d8826ffc575c4e3
Sophart <phunsophart@gmail.com>
2020-10-04 22:16:26 +0700

    Request Hierachy Data - MembershipType in Customer
    The name MembershipType here in CustomerDto is important. Must be MembershipType not MembershipTypeDto.

--- Vidly/Controllers/Api/CustomersController.cs
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web.Http;
@@ -26,7 +27,11 @@ namespace Vidly.Controllers.Api
         //GET /api/customers
         public IHttpActionResult GetCustomers()
         {
-            return Ok(_dbContext.Customers.ToList().Select(Mapper.Map<Customer, CustomerDto>));
+            return Ok(_dbContext.Customers
+                .Include(c => c.MembershipType)
+                .ToList()
+                .Select(Mapper.Map<Customer, CustomerDto>)
+            );
         }
 
         //GET /api/customers/[id]
